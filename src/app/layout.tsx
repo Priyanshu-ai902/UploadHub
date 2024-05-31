240b870b3d363365365c743b4d237e92969b5a3f
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-05-31 18:31:52 +0530

    setup of project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "@/ConvexClientProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets:
+  ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,43 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+
+} from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignInButton mode="modal"></SignInButton>
-     <Button>Hello world</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button>sign out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>sign in</Button>
+        </SignInButton>
+      </SignedOut>
+
+      {files?.map((file) => {
+        return <div key={file._id}>{file.name}</div>;
+      })}
+
+      
+      <Button onClick={() => {
+        createFile({
+          name: "hello world",
+        })
+      }}>clickme</Button>
     </main>
   );
 }
