0fd0f94bf543a63e39f82ab18b1fb98c024c67f2
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-08-04 01:24:29 +0530

    uploading file to and saving to database

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export function Header() {
-    return <div className="border-b py-4 bg-gray-50">
+    return <div className="border-b py-3 bg-gray-50">
         <div className="items-center container mx-auto justify-between flex">
-            <div className="">
-                UploadHub
+            <div className=" font-bold flex justify-between items-center text-lg">
+                <Image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYm134bCIqPIdxE0uUFIdhd_YyB1RzVJKqIw&s" alt="logo" width={40}
+                    height={40} />
+                Upload
+                <span className="text-blue-500">Hub</span>
             </div>
             <div className=" flex gap-2">
                 <OrganizationSwitcher />
