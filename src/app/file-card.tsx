56a218c21be578ed288b385b845d517a017febe6
Priyanshu-ai902 <jhapriyanshus111@gmail.com>
2024-08-04 19:06:07 +0530

    delete functionality added for the file

---
+++ src/app/file-card.tsx
@@ -0,0 +1,105 @@
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Doc } from "../../convex/_generated/dataModel"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MoreVerticalIcon, Trash2 } from "lucide-react";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import { useState } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useToast } from "@/components/ui/use-toast";
+
+
+
+function FileCardActions({ file }: { file: Doc<"files"> }) {
+    const { toast } = useToast();
+    const deleteFile = useMutation(api.files.deleteFile)
+    const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+
+    return (
+        <>
+            <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete your file
+                            and and you no longer access of it.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction onClick={async () => {
+                            await deleteFile({
+                                fileId: file._id,
+                            });
+                            toast({
+                                variant: "default",
+                                title: "File deleted ",
+                                description: "Your file is deleted successfully",
+                                className: "bg-red-500 text-white"
+                            });
+                        }}>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                    <MoreVerticalIcon />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem onClick={() => setIsConfirmOpen(true)}
+                        className="flex gap-1 text-red-500 items-center cursor-pointer">
+                        <Trash2 className="w-4 h-4" /> Delete
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    );
+
+}
+
+
+export function FileCard({ file }: { file: Doc<"files"> }) {
+    return (
+        <Card>
+            <CardHeader className="relative">
+                <CardTitle>{file.name}
+                </CardTitle>
+                <div className="absolute top-2 right-2">
+                    <FileCardActions file={file} />
+                </div>
+
+                {/* <CardDescription>Card Description</CardDescription> */}
+            </CardHeader>
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <Button>Download</Button>
+            </CardFooter>
+        </Card>
+
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,195 +1,40 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, SignOutButton, useOrganization, useUser } from "@clerk/nextjs";
-import { useMutation, useQuery } from "convex/react";
+import { useOrganization, useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
-
-import { z } from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
+import { UploadButton } from "./upload-button";
+import { FileCard } from "./file-card";
 
 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useState } from "react";
-import { useToast } from "@/components/ui/use-toast";
-import { LoaderCircle } from "lucide-react";
-
-
-const formSchema = z.object({
-  title: z.string().min(1).max(20),
-  file: z
-    .custom<FileList>((val) => val instanceof FileList, "Required").refine((files) => files.length > 0, `Required`),
-})
 
 export default function Home() {
-  const { toast } = useToast();
   const organization = useOrganization();
   const user = useUser()
-  const generateUploadUrl = useMutation(api.files.generateUploadUrl)
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      file: undefined,
-    },
-  })
-
-  const fileRef = form.register("file")
-
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values)
-    console.log(values.file)
-    if (!orgId) return;
-
-    const postUrl = await generateUploadUrl();
-
-    const result = await fetch(postUrl, {
-      method: "POST",
-      headers: { "Content-Type": values.file[0].type },
-      body: values.file[0],
-    });
-    const { storageId } = await result.json()
-
-    try {
-      await createFile({
-        name: values.title,
-        fileId: storageId,
-        orgId
-      });
-
-      form.reset();
-      setIsFileDialogOpen(false)
-
-      toast({
-        variant: "default",
-        title: "File Uploaded",
-        description: "Now everyone can view your file",
-        className: "bg-purple-500 text-black"
-      })
-    } catch (err) {
-      toast({
-        variant: "default",
-        title: "Something went wrong",
-        description: "Your file is not uploaded ,try again",
-        className: "bg-red-500 text-white"
-      })
-    }
-
-
-  }
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
 
-  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false)
-
 
   const files = useQuery(api.files.getFiles,
     orgId ? { orgId } : "skip");
-  const createFile = useMutation(api.files.createFile)
+
 
   return (
     <main className="container mx-auto pt-7">
       <div className="flex justify-between items-center">
         <h1 className="text-3xl font-bold">Your Files</h1>
-
-        <Dialog
-          open={isFileDialogOpen}
-          onOpenChange={(isOpen) => {
-            setIsFileDialogOpen(isOpen);
-            form.reset();
-          }}
-        >
-          <DialogTrigger asChild>
-
-            <Button onClick={() => {
-
-            }}>Upload File</Button>
-
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle className="mb-4">Uplaod your file here</DialogTitle>
-              <DialogDescription>
-                This file will be accessible by anyone in your organization
-              </DialogDescription>
-            </DialogHeader>
-
-            <Form {...form}>
-              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-                <FormField
-                  control={form.control}
-                  name="title"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Title</FormLabel>
-                      <FormControl>
-                        <Input {...field} />
-                      </FormControl>
-
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="file"
-                  render={({ field: { onChange }, ...field }) => (
-                    <FormItem>
-                      <FormLabel>File</FormLabel>
-                      <FormControl>
-                        <Input
-
-                          type="file" {...fileRef}
-
-                        />
-                      </FormControl>
-
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <Button type="submit"
-                  disabled={form.formState.isSubmitting}
-                  className="flex gap-1">
-                    {form.formState.isSubmitting&& (
-                      <LoaderCircle className="h-4 w-4 animate-spin"/>
-                    )}
-                    Submit</Button>
-              </form>
-            </Form>
-
-          </DialogContent>
-        </Dialog>
+        <UploadButton />
       </div >
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })
-      }
 
-
+      <div className="grid grid-cols-4 gap-2 mt-3">
+        {files?.map((file) => {
+          return <FileCard key={file._id} file={file} />
+        })
+        }
+      </div>
     </main >
   );
 }
